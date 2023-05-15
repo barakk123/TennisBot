9ba56a2e426cd1403db3a658cb9c0ff1c5e19a95
barakk123 <71881281+barakk123@users.noreply.github.com>
2023-05-15 23:20:15 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -18,43 +18,80 @@ $(document).ready(function() {
 });
 
 function complete() {
+
   localStorage.clear();
+
   let NameOfGoal = document.querySelector('#nameOfGoal').value;
   let startDate = document.querySelector('#startDate').value;
   let endDate = document.querySelector('#endDate').value;
 
-  console.log(NameOfGoal, startDate, endDate);
-
   localStorage.name = NameOfGoal;
   localStorage.startDate = startDate;
   localStorage.endDate = endDate;
-  localStorage.forHand = forHand;
-  localStorage.backhand = backhand;
-  localStorage.volley = volley;
-  localStorage.slice = slice;
-  localStorage.dropshot = dropshot;
-
-
-  console.log(localStorage);
+  
 }
 
 function save() {
   let s1 = document.querySelector('#s1').value;
   let a1 = document.querySelector('#a1').value;
+  let s2 = document.querySelector('#s2').value;
+  let a2 = document.querySelector('#a2').value;
+  let s3 = document.querySelector('#s3').value;
+  let a3 = document.querySelector('#a3').value;
+  let s4 = document.querySelector('#s4').value;
+  let a4 = document.querySelector('#a4').value;
+  let s5 = document.querySelector('#s5').value;
+  let a5 = document.querySelector('#a5').value;
 
   let forHand = {
     startSpeed: 65,
+    accuracyStart: 62,
     speed: s1,
-    averageSpeed: a1,
+    accuracy: a1,
+  };
+  
+  let backHand = {
+    startSpeed: 73,
+    accuracyStart: 68,
+    speed: s2,
+    accuracy: a2,
   };
 
+  let volley = {
+    startSpeed: 92,
+    accuracyStart: 59,
+    speed: s3,
+    accuracy: a3,
+  };
+
+  let slice = {
+    startSpeed: 60,
+    accuracyStart: 55,
+    speed: s4,
+    accuracy: a4,
+  };
+
+  let dropshot = {
+    startSpeed: 50,
+    accuracyStart: 47,
+    speed: s5,
+    accuracy: a5,
+  }
+
   let jfh = JSON.stringify(forHand);
+  let jbh = JSON.stringify(backHand);
+  let jv = JSON.stringify(volley);
+  let js = JSON.stringify(slice);
+  let jd = JSON.stringify(dropshot);
 
   localStorage.forHand = jfh;
-
-  console.log(localStorage);
+  localStorage.backHand = jbh;
+  localStorage.volley = jv;
+  localStorage.slice = js;
+  localStorage.dropshot = jd;
 
   document.getElementById("mod").style.display = "block";
+
 }
 
 function go() {
@@ -62,7 +99,7 @@ function go() {
   let startDate = localStorage.startDate;
   let endDate = localStorage.endDate;
   let forHand = localStorage.forHand;
-  let backhand = localStorage.backhand;
+  let backHand = localStorage.backHand;
   let volley = localStorage.volley;
   let slice = localStorage.slice;
   let dropshot = localStorage.dropshot;
@@ -76,7 +113,7 @@ function go() {
     <br>
     Forhand: ${forHand}
     <br>
-    Backhand: ${backhand}
+    Backhand: ${backHand}
     <br>
     Volley: ${volley}
     <br>
